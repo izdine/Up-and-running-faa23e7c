730d32a993691e7fd85ecaf13621e2447da9ac5b
Iz-Dine Amadou <2088346@talnet.nl>
2020-03-19 12:24:33 +0100

    Uitwerking van de exercise

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$host = 'localhost';
+$db   = 'netland';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+?>
+<?php
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+   }
+   $status = $pdo->getAttribute(PDO::ATTR_SERVER_VERSION);
+   var_dump($status);
+?>
